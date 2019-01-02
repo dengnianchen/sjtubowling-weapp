df8a8e3549a5533275610a1be8a956fba7f64822
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-01-02 09:48:22 +0800

    整合重构代码（进行中）：添加类User，Session，封装相关操作
    Signed-off-by: dengnianchen <dengnianchen@sjtu.edu.cn>

---
+++ src/models/user.js
@@ -0,0 +1,60 @@
+const utils = require('../utils/index');
+const config = require('../config');
+
+class User {
+	
+	constructor(userData) {
+		utils.extend(this, userData);
+		this._preprocess();
+	}
+	
+	checkInfoCompletion() {
+		return [
+			this.info.gender !== 0,
+			this.info.photo !== config.defaultAvatar,
+			!this.active || this.numOfFreeTimeSelected() >= getApp().globalData.config.timetable_min_selected
+		];
+	}
+	
+	numOfFreeTimeSelected() {
+		let n = 0;
+		for (let key in this.timetable)
+			n += this.timetable[key];
+		return n;
+	}
+	
+	/**
+	 * 对服务器传回的用户信息进行预处理：
+	 * 1. 预处理年级显示格式为统一的三位形式，空显示为未知
+	 * 2. 转化头像标识为缩略图路径和大图路径方便界面层显示
+	 *
+	 * @author Deng Nianchen
+	 */
+	_preprocess() {
+		if (this.grade !== undefined) {
+			if (!this.grade)
+				this.grade = '未知';
+			else
+				this.grade = this.grade.substr(0, 3);
+		}
+		if (this.info && this.info.photo !== undefined) {
+			if (!this.info.photo) {
+				this.info.fullPhoto = config.defaultAvatar;
+				this.info.thumbnail = config.defaultAvatar;
+			} else {
+				this.info.fullPhoto = config.avatarUrl + this.info.photo.substr(7);
+				this.info.thumbnail = config.avatarUrl + this.info.photo;
+			}
+		}
+		if (this.photo !== undefined) {
+			if (!this.photo) {
+				this.fullPhoto = config.defaultAvatar;
+				this.thumbnail = config.defaultAvatar;
+			} else {
+				this.fullPhoto = config.avatarUrl + this.photo.substr(7);
+				this.thumbnail = config.avatarUrl + this.photo;
+			}
+		}
+	}
+	
+}
