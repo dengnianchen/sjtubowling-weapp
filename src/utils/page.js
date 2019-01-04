a766a37965e01b3cf5438bd3a06deaabd29dfd66
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-01-04 16:33:27 +0800

    weapp: 代码重构中
    Signed-off-by: dengnianchen <dengnianchen@sjtu.edu.cn>

--- src/utils/page.js
+++ src/utils/page.js
@@ -15,6 +15,13 @@ module.exports = {
 		return path;
 	},
 	
+	getPathEncoded() {
+		let path = "/" + this.route;
+		if (!$(this.options).isEmpty())
+			path += `?${$(this.options).urlParams()}`;
+		return encodeURIComponent(path);
+	},
+	
 	setLoading(isLoading, ex) {
 		if (isLoading)
 			this.setData({ loading: true, loadingError: null });
