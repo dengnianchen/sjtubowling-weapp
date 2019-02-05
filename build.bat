f747f434b4cac8234451cb15aaf2337aa8485ce4
dengnianchen <dengnianchen@sjtu.edu.cn>
2019-02-05 13:06:25 +0800

    组件抽离进行中：使用新组件wui-panel

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 tskill wechatdevtools
 cmd /C min build
 copy /Y src\images dist\images
-cmd /C "C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" -o E:/My_Project/sjtubowling-weapp
+cmd /C "C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" -o %~dp0
